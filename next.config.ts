325a730ca9571222871febcfa05e388df0d6d067
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2026-07-13 10:29:25 -0400

    [TEST] Removendo configurações para testar PWA
    O commit passado não funcionou, testando se PWA é ativado já com o manifest e ícones.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
